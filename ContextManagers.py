3d202460e87b6182d95e93d2da3c45967cc8f96e
forrex322 <zonex1995@gmail.com>
2022-04-13 16:11:21 +0300

    reworked context manager, added implemention of contexmanager decorator, reworked sorting

--- ContextManagers.py
+++ ContextManagers.py
@@ -1,30 +1,44 @@
 import csv
+from contextlib import contextmanager
+
+
+class ContextManagerAppender(object):
+
+    def __init__(self, file_name):
+        if not file_name.endswith('.csv'):
+            print('File must be .csv')
+            raise Exception('File must be .csv')
+        self.file_name = file_name
+
+    def __enter__(self):
+        self.file = open(self.file_name, 'a', newline='')
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.file.close()
+
+
+@contextmanager
+def append_file(file_name):
+    if not file_name.endswith('.csv'):
+        print('File must be .csv')
+        raise Exception('File must be .csv')
+
+    file = open(file_name, mode='a', newline='')
+    try:
+        yield file
+    finally:
+        file.close()
+
 
 filename = 'test.csv'
 
-print('Writing to file')
+data3 = [[f'Bob; {22}'], [f'Bib; {20}'], [f'Bab; {23}']]
+with ContextManagerAppender(filename) as xfile:
+    csvappender = csv.writer(xfile)
+    csvappender.writerows(data3)
 
-header = ['Name; Age']
-data = [[f'Alex; {18}'], [f'Dima; {20}'], [f'Oleg; {24}']]
-
-with open(filename, 'w', newline='') as file:
-    csvwriter = csv.writer(file)
-    csvwriter.writerow(header)
-    csvwriter.writerows(data)
-
-print('Reading from file')
-
-rows = []
-with open(filename, 'r') as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        rows.append(row)
-        print(row)
-print(rows)
-
-print('Appending to file')
-
-data2 = [[f'Maks; {22}'], [f'Vova; {20}']]
-with open(filename, 'a', newline='') as file:
+data4 = [[f'Foo; {22}'], [f'Baz; {20}'], [f'Bar; {23}']]
+with append_file(filename) as file:
     csvappender = csv.writer(file)
-    csvappender.writerows(data2)
+    csvappender.writerows(data4)
--- Sorts.py
+++ Sorts.py
@@ -1,6 +1,6 @@
 from operator import itemgetter
 
-print(sorted([(3, 2, 3, 5), (5, 1, 5, 2, 5, 7, 8), (2, 7)], key=lambda x: x[-1], reverse=True))
+print(sorted([(3, 2, 3, 5), (2, 1), (5, 1, 5, 2, 5, 7, 8), (2, 7)], key=lambda x: x[-1], reverse=True))
 
 
 def sort_without_z(s):
@@ -14,3 +14,6 @@ def sort_without_z(s):
 
 
 print(sort_without_z(['zxc', 'xzc', 'zcx', 'cxz', 'zxx', 'vbv']))
+
+l = ['zxc', 'xzc', 'zcx', 'cxz', 'zxx', 'vbv']
+print(sorted(l, key=lambda x: (not x.startswith('z'), x)))
